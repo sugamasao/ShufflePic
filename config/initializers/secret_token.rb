3f2e82cd93a808a052a0c1c2e8df405411a52f1e
sugamasao <sugamasao@gmail.com>
2012-03-06 08:34:29 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RandomTweet::Application.config.secret_token = '07374b1c64bed3aa74ebd5ac443336eaa2f5fb0c42f97f98654e856637275002c74202fb66314eecb83d744c5f60356c3a96093ba0e76ca0870787f0de3fe8b8'
